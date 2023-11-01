248f8d8e7643a1c3b76436c04ce60cee53f82051
jonathan <jonathansusanto2015@gmail.com>
2023-11-01 17:30:10 +0700

    first commit

---
+++ resources/views/FrontEnd/TK.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Edit Sini Ya mantemam Frontenders</h1>
+@stop
+
+@section('footer')
+    <h1>Ini biarin aja</h1>
+@stop
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    {{-- Ini Yang Di Edit Ya Front Enders --}}
+    <div class="container mx-6 mt-6">
+        <h1 class="text-3xl font-bold underline">Hello World</h1>
+        @yield('content')
+        @yield('footer')
+    </div>
+</body>
+</html>

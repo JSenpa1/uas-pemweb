ec8ee584feace87a593b074ba2e78f6f95b0691c
jonathan <jonathansusanto2015@gmail.com>
2023-11-18 18:17:19 +0700

    uwu

--- resources/views/FrontEnd/TK.blade.php
+++ resources/views/FrontEnd/TK.blade.php
@@ -5,7 +5,7 @@
         <img src="{{ asset('classroom_1542034403442_61914801_ver1.0.webp') }}" class="sm:w-46 md:h-96 md:w-full object-cover" alt="classroomImage"/>
     </div>
     <div>
-        <h1>kartu Kartu</h1>
+        <h1>Nanti Bikin yang kartu disini</h1>
     </div>
 @stop
 

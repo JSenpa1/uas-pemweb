263ae4d3c2a4eac11020bbdc1d9d5362236a1f6b
jonathan <jonathansusanto2015@gmail.com>
2023-11-18 18:07:33 +0700

    modif frontend dikit

--- resources/views/FrontEnd/TK.blade.php
+++ resources/views/FrontEnd/TK.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>Ini Konten page</h1>
+    <div>
+        <img src="{{ asset('classroom_1542034403442_61914801_ver1.0.webp') }}" class="sm:w-46 md:h-96 md:w-full object-cover" alt="classroomImage"/>
+    </div>
+    <div>
+        <h1>kartu Kartu</h1>
+    </div>
 @stop
 
 

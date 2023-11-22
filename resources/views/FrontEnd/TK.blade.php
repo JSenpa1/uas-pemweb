0b57afb92816a78a0901cc113a1ca580109657e3
Gynthunder <maryadigt@gmail.com>
2023-11-22 16:35:26 +0700

    homepage
    baru gan

--- resources/views/FrontEnd/TK.blade.php
+++ resources/views/FrontEnd/TK.blade.php
@@ -4,9 +4,103 @@
     <div>
         <img src="{{ asset('classroom_1542034403442_61914801_ver1.0.webp') }}" class="sm:w-46 md:h-96 md:w-full object-cover" alt="classroomImage"/>
     </div>
-    <div>
-        <h1>Nanti Bikin yang kartu disini</h1>
+<div class="container my-24 mx-auto md:px-6" style="background-color:#87BD2F;">
+  <section class="mb-32">
+    <div class="mb-16 flex flex-wrap">
+      <div class="mb-6 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-6/12 lg:pr-6">
+        <div
+          class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20"
+          data-te-ripple-init data-te-ripple-color="light">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/028.jpg" class="w-full" alt="Louvre" />
+          <a href="#!">
+            <div
+              class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 lg:pl-6">
+      <h3 class="mb-4 text-2xl font-bold">Aktivitas menyenangkan!</h3>
+        <div class="mb-4 flex items-center text-sm font-medium text-primary dark:text-primary-400">
+        <p class="mb-6">
+          Ut pretium ultricies dignissim. Sed sit amet mi eget urna placerat
+          vulputate. Ut vulputate est non quam dignissim elementum. Donec a
+          ullamcorper diam.
+        </p>
+      </div>
     </div>
+
+    <div class="mb-16 flex flex-wrap lg:flex-row-reverse">
+      <div class="mb-6 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-6/12 lg:pl-6">
+        <div
+          class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20"
+          data-te-ripple-init data-te-ripple-color="light">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/033.jpg" class="w-full" alt="Louvre" />
+          <a href="#!">
+            <div
+              class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 lg:pr-6">
+        <h3 class="mb-4 text-2xl font-bold">Sekolah Al-Quran</h3>
+        <div class="mb-4 flex items-center text-sm font-medium text-primary dark:text-primary-400">
+        <p>
+          Duis sagittis, turpis in ullamcorper venenatis, ligula nibh porta
+          dui, sit amet rutrum enim massa in ante. Curabitur in justo at
+          lorem laoreet ultricies. Nunc ligula felis, sagittis eget nisi
+          vitae, sodales vestibulum purus. Vestibulum nibh ipsum, rhoncus
+          vel sagittis nec, placerat vel justo. Duis faucibus sapien eget
+          tortor finibus, a eleifend lectus dictum. Cras tempor convallis
+          magna id rhoncus. Suspendisse potenti. Nam mattis faucibus
+          imperdiet. Proin tempor lorem at neque tempus aliquet. Phasellus
+          at ex volutpat, varius arcu id, aliquam lectus. Vestibulum mattis
+          felis quis ex pharetra luctus. Etiam luctus sagittis massa, sed
+          iaculis est vehicula ut.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-wrap">
+      <div class="mb-6 w-full shrink-0 grow-0 basis-auto lg:mb-0 lg:w-6/12 lg:pr-6">
+        <div
+          class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat shadow-lg dark:shadow-black/20"
+          data-te-ripple-init data-te-ripple-color="light">
+          <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/079.jpg" class="w-full" alt="Louvre" />
+          <a href="#!">
+            <div
+              class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="w-full shrink-0 grow-0 basis-auto lg:w-6/12 lg:pl-6">
+      <h3 class="mb-4 text-2xl font-bold">Anak-anak yang gembira</h3>
+    <div class="mb-4 flex items-center text-sm font-medium text-primary dark:text-primary-400">
+        <p>
+          Sed sollicitudin purus sed nulla dignissim ullamcorper. Aenean
+          tincidunt vulputate libero, nec imperdiet sapien pulvinar id.
+          Nullam scelerisque odio vel lacus faucibus, tincidunt feugiat
+          augue ornare. Proin ac dui vel lectus eleifend vestibulum et
+          lobortis risus. Nullam in commodo sapien. Curabitur ut erat congue
+          sem finibus eleifend egestas eu metus. Sed ut dolor id magna
+          rutrum ultrices ut eget libero. Duis vel porttitor odio. Ut
+          pulvinar sed turpis ornare tincidunt. Donec luctus, mi euismod
+          dignissim malesuada, lacus lorem commodo leo, tristique blandit
+          ante mi id metus. Integer et vehicula leo, vitae interdum lectus.
+          Praesent nulla purus, commodo at euismod nec, blandit ultrices
+          erat. Aliquam eros ipsum, interdum et mattis vitae, faucibus vitae
+          justo. Nulla condimentum hendrerit leo, in feugiat ipsum
+          condimentum ac. Maecenas sed blandit dolor.
+        </p>
+      </div>
+    </div>
+  </section>
+</div>
 @stop
 
 
